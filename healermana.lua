923d5a7e4f53998d3f15b547242238cfb2dcd8bf
zsoltfrks <zsoltfrks@icloud.com>
2026-03-12 11:01:36 +0100

    feat: add default frame position

--- healermana.lua
+++ healermana.lua
@@ -1,1 +1,15 @@
+local addon = CreateFrame("Frame")
+local frames = {}
+local healers = {}
+
+-- Default position
+local defaultPosition = {
+    point = "CENTER", 
+    x = 0,
+    y = 200
+}
+
+
+
+
 
